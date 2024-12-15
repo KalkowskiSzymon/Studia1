42936d6d27621a751bd2bc36e0a18626226e2d84
Szymon Kałkowski <kalkowskiszymon222@gmail.com>
2024-12-15 15:31:53 +0100

    black

--- zad 2.py
+++ zad 2.py
@@ -1,19 +1,22 @@
-#A
+# A
 def display_names(names):
     for name in names:
         print(name)
 
+
 display_names(["Anna", "Piotr", "Kasia", "Marek", "Tomek"])
 
 
-#B
+# B
 def multiply_by_two_loop(numbers):
     result = []
     for num in numbers:
         result.append(num * 2)
     return result
 
+
 print(multiply_by_two_loop([1, 2, 3, 4, 5]))
+
 
 def multiply_by_two_comprehension(numbers):
     return [num * 2 for num in numbers]
@@ -22,7 +25,7 @@ def multiply_by_two_comprehension(numbers):
 print(multiply_by_two_comprehension([1, 2, 3, 4, 5]))
 
 
-#C
+# C
 def display_even_numbers(numbers):
     for number in numbers:
         if number % 2 == 0:
@@ -31,10 +34,12 @@ def display_even_numbers(numbers):
 
 display_even_numbers(range(10))
 
-#D
+# D
+
 
 def display_every_second(numbers):
     for i in range(0, len(numbers), 2):
         print(numbers[i])
+
 
 display_every_second(range(10))
